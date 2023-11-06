fca616ea856ef0728ad34b50affe238078b255cf
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-11-06 16:31:00 +0700

    fix typo
    import {firstore} to {firestore}

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvier } from "firebase/auth";
-import { firstore } from "firebase/firestore";
+import { firestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
