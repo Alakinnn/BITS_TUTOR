2568c96071c2add7da667d94816fa87e52cd3130
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-11-13 23:31:52 +0700

    added UUID, update err handler, multer logics

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,16 +1,36 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from "express"
+import multer from "multer"
 
 interface APIError extends Error{
     statusCode: number,
 }
 
 const errorHandler: ErrorRequestHandler = (err: APIError, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof multer.MulterError) {
+        if (err.code === "LIMIT_FILE_SIZE") {
+            return res.status(400).json({
+              message: "file is too large",
+            });
+          }
+      
+          if (err.code === "LIMIT_FILE_COUNT") {
+            return res.status(400).json({
+              message: "File limit reached",
+            });
+          }
+      
+          if (err.code === "LIMIT_UNEXPECTED_FILE") {
+            return res.status(400).json({
+              message: "File must be an image",
+            });
+    }
     const customError = {
         statusCode: err.statusCode || 500,
         message: err.message || "Something went wrong, please try again later" 
     }
 
     res.status(customError.statusCode).json({message: customError.message})
+    }
 }
 
 export default errorHandler
--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,25 +1,48 @@
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
 import {Response, Request} from "express"
-
+import { v4 as uuid } from 'uuid';
 
 type req = Request
 type res = Response
 
-const storage = multer.memoryStorage()
+// Guidelines:
+// 1. Multer takes in the destination and the filename (declared as storage atm)
+// 2. Depends on the route, multer is going to filter file types and sizes
+// 3. Add fields and the maximum file for each field
 
-const fileFilter = (req: Request, file: any, cb: any) => {
-  if (file.mimetype.split("/")[0] === "image") {
-    cb(null, true);
-  } else {
-    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
-  }
-};
+// const storage = multer.memoryStorage()
 
+const storage = multer.diskStorage({
+  destination: (req: req, file: any, cb: any) => {
+    cb(null, "Uploads ")
+  },
+  filename(req: req, file: any, cb: any) {
+      const originName = file
+      cb(null, `${uuid()}-${originName}`)
+  },
+})
+
+// Sample Filter
+// const fileFilter = (req: Request, file: any, cb: any) => {
+//   if (file.mimetype.split("/")[0] === "image") {
+//     cb(null, true);
+//   } else {
+//     cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
+//   }
+// };
+
+// Todo: Ask FE how many types a student && tutor can upload to the system
 // This is currently for image
 const uploadImage = multer({
   storage,
-  fileFilter,
-  limits: { fileSize: 1000000000, files: 1 },
+  fileFilter(req, file, callback) {
+    if (file.mimetype.split("/")[0] === "image") {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  limits: { fileSize: 1000000000, files: 1 }
 })
 
 // TODO: what the user upload is going to depend on the route, theses are only DEVELOPMENT variables
