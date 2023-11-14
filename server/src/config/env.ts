16bf61fef94866db0bc786a1ff9785249e375cc3
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-11-14 10:26:06 +0700

    upload to S3 configured

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -8,15 +8,11 @@ const env = {
     PORT: process.env.PORT || 3000, // Should not provide port directly since this is taken care of by NGINX
     MONGO_PORT: process.env.MONGO_PORT || 27017,    // Should just keep default port for MongoD
     MONGO_IP: process.env.MONGO_IP || "database",   // Using DNS to resolve service IP address
-
-    // S3
-
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_KEY_ID: process.env.AWS_SECRET_KEY,
-
-    DO_S3_REGION: process.env.DO_REGION,
-    DO_S3_NAME: process.env.DO_S3_NAME
     
+    // S3
+    DO_S3_REGION: process.env.DO_REGION || "SGP1",
+    DO_S3_ENDPOINT: process.env.DO_ENDPOINT || "https://finder-tutor.sgp1.digitaloceanspaces.com",
+    DO_S3_NAME: process.env.DO_NAME || "finder-tutor"
 }
 
 export default env
--- server/src/db/s3connect.ts
+++ server/src/db/s3connect.ts
@@ -1,7 +1,29 @@
-import {S3} from "aws-sdk";
+
+import multer from "multer"
+import multerS3 from "multer-s3"
+import env from "../config/env"
+import { S3Client } from "@aws-sdk/client-s3";
+import { fromEnv } from "@aws-sdk/credential-providers"
+import {v4 as uuid} from "uuid"
+const {DO_S3_REGION, DO_S3_ENDPOINT, DO_S3_NAME} = env
 
 
-export const s3uploadV2 =async () => {
-  const s3 = new S3()
-  
-}
+const S3 = new S3Client(
+  {
+    region: DO_S3_REGION,
+    credentials: fromEnv(),
+    endpoint: DO_S3_ENDPOINT
+  }
+)
+
+
+export const upload = multer ({
+  storage: multerS3({
+    s3: S3,
+    bucket: DO_S3_NAME,
+    acl: "public-read",
+    key: function (req, file, cb) {
+        cb(null, `${uuid()}-${file.originalname}`)
+    }
+  })
+})
