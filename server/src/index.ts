8e16b5586dab3454506e624858e9937c4bde1ff4
MasterMobo <mastermobo101@gmail.com>
2023-11-28 21:49:57 +0700

    refactored imports

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,37 @@
-import 'express-async-errors'
+import "express-async-errors";
 
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
-import cors from 'cors'
+import cors from "cors";
 
-import env from './config/env'
-import authRouter from './routes/authRoutes'
-import tutorRouter from './routes/tutorRoutes'
-import studentRouter from './routes/studentRoutes'
-import reviewRouter from './routes/reviewRoutes'
-import errorHandler from './middlewares/errorHandler'
-import { connectDB } from './db/connect'
+import env from "./config/env";
+import {
+  authRouter,
+  tutorRouter,
+  studentRouter,
+  reviewRouter,
+} from "./routes/index";
 
+import { errorHandler } from "./middlewares/index";
 
-app.enable("trust proxy")
+import { connectMongoDB } from "./db/connect";
 
-app.use(cors())
-app.use(express.json())
+app.enable("trust proxy");
+
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/tutor", tutorRouter)
-app.use("/api/v1/student", studentRouter)
-app.use("/api/v1/review", reviewRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/tutor", tutorRouter);
+app.use("/api/v1/student", studentRouter);
+app.use("/api/v1/review", reviewRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
-    await connectDB();
-    app.listen(env.PORT, () => console.log(`Listening on port ${env.PORT}!`))
-}
-start()
+  await connectMongoDB();
+  app.listen(env.PORT, () => console.log(`Listening on port ${env.PORT}!`));
+};
+start();
---
+++ server/src/routes/index.ts
@@ -0,0 +1,6 @@
+import authRouter from "./authRoutes";
+import tutorRouter from "./tutorRoutes";
+import studentRouter from "./studentRoutes";
+import reviewRouter from "./reviewRoutes";
+
+export { authRouter, tutorRouter, studentRouter, reviewRouter };
