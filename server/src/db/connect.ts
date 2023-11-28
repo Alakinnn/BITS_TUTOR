e915c3ae11f10649120b1a9105e0beac6d0425a2
MasterMobo <mastermobo101@gmail.com>
2023-11-28 22:04:29 +0700

    fixed connectMongoDB error

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
-import env from '../config/env'
-const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, MONGO_DB_NAME } = env
+const mongoose = require("mongoose");
+import env from "../config/env";
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, MONGO_DB_NAME } =
+  env;
 
-const connectUri =`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin` 
+const connectUri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(connectUri)
-        console.log("Successfully connected to MongoDB!");
-    } catch (e) {
-        console.log(e);
-    }
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(connectUri);
+    console.log("Successfully connected to MongoDB!");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-}
-
-export {connectDB, connectUri}
+export { connectMongoDB, connectUri };
