5d87b2b9c3c5ded2edbfe2fa3e21d17fde1c71e5
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-11-29 02:37:43 +0700

    getZoomInitData created

--- server/src/controllers/sessionController.ts
+++ server/src/controllers/sessionController.ts
@@ -1,4 +1,5 @@
 // import {SessionModel} from "../schema/sessionSchema" => I'm just going to assume there exists such model
+import axios from "axios"
 import { Request, Response } from "express"
 const updateLiveCodingURL = (req: Request, res: Response) => {
   try {
@@ -15,6 +16,36 @@ const updateLiveCodingURL = (req: Request, res: Response) => {
       res.status(200).json({
         message: "Live Coding URL submitted successfully"
       }) 
+    }
+  } catch (error) {
+    res.status(400).json(
+      {message: error}
+    )
+  }
+}
+
+const getZoomInitData = async (req: Request, res: Response) => {
+  try {
+    if(req?.params?.id) {
+      res.status(404).json({
+        message: "Session Not Found"
+      })
+    }
+    // const session = await SessionModel.findOne(req.params.id).exec()
+    const session: Object= Error // Delete this later once there is model
+    if (session) {
+      // const hostEmail = session.owner
+      // const zoomAccessToken = await axios.get(`https://api.zoom.us/v2/users/${hostEmail}/meetings`)
+      // const meetingNumber = session.meetingID
+
+      const zoomData = {
+        // meetingNumber: meetingNumber,
+        // userName: hostEmail => I just realized that we have username values so may query that instead of using email
+        // zak: zoomAccessToken
+      }
+      res.status(200).json({
+        zoomData
+      })
     }
   } catch (error) {
     res.status(400).json(
