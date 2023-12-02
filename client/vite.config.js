ca8d989507884a7f1fc44d06ab330c25f3a4e757
MasterMobo <mastermobo101@gmail.com>
2023-12-03 01:07:16 +0700

    client is now containerized

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: "0.0.0.0",
         port: 5173,
+        watch: {
+            usePolling: true,
+        },
     },
     preview: {
+        host: "0.0.0.0",
         port: 5173,
     },
 });
