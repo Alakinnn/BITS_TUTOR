14b21be650e6ea07619ef0ac0473f3d50933fa38
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-03 20:45:41 +0700

    Fix student, tutor interface & navbar

--- client/src/tutorInterface.jsx
+++ client/src/tutorInterface.jsx
@@ -63,61 +63,133 @@ const tutorInterface = () => {
     }    
 
     return (
-    <>
-        <div className='headTitle'>
-        <h1>Meeting 68757</h1>
-        <div className='student'>
-            <a href="/student"><img className='logo' src="./images/address.png" alt='student'></img></a>
-            <p>Student</p>
+        <>
+        <div id="boxContains">
+          <div className="headTitle">
+            <div className="container">
+            <div className="opacity"></div>
+              <img className="hinddenPic" src={hidden} alt="" />
+              <h2>Meeting 68757</h2>
+            </div>
+  
+            <div className="imgBox">
+              <div className="student">
+                <a href="/student">
+                  <img className="studentImg" src={stuImg} alt="student" />
+                </a>
+                <p>Name</p>
+              </div>
+              <div className="tutor">
+                <a href="/tutor">
+                  <img
+                    className="tutorImg"
+                    src={"src/assets/footer/linkIn.svg"}
+                    alt="tutor"
+                    onClick={tutorInterface}
+                  />
+                </a>
+                <p>Name</p>
+              </div>
+            </div>
+          </div>
+  
+          <div className="infoDate">
+            <h3 className="title">Date :</h3>
+            <h3 className="text">Mon 09/11/2924</h3>
+          </div>
+  
+          <div className="infoStart">
+            <h3 className="title">Start Time :</h3>
+            <h3 className="text">12am PCT</h3>
+          </div>
+  
+          <div className="infoEnd">
+            <h3 className="title">End Time :</h3>
+            <h3 className="text">2am PCT</h3>
+          </div>
+  
+          <h4 id="message">To start the meeting, enter Live Share URL</h4>
+  
+          {session.status === "ongoing" ? (
+            <div className="endSession">
+              <button onClick={endSession}>End Session</button>
+            </div>
+          ) : (
+            <div className="startSession">
+              <input
+                type="url"
+                placeholder="Live Share URL"
+                onChange={handleChange}
+              />
+              <button onClick={startSession}>Start Session</button>
+            </div>
+          )}
         </div>
-        <div className='tutor'>
-            <a href="/tutor"><img className='logo' src="./images/address.png" alt='tutor'></img></a>
-            <p>Tutor</p>
-        </div>
-      </div>
+  
+        {/* <h1>Tutor's View</h1>
+        <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
+        <button onClick={startSession}>Start Session</button>
+        <p>Live Share URL: {inputUrl}</p>
+        
+        <h1>Student's View</h1>
+        <button onClick={joinSession}>Join Session</button> */}
+      </>
+
+    // <>
+    //     <div className='headTitle'>
+    //     <h1>Meeting 68757</h1>
+    //     <div className='student'>
+    //         <a href="/student"><img className='logo' src="./images/address.png" alt='student'></img></a>
+    //         <p>Student</p>
+    //     </div>
+    //     <div className='tutor'>
+    //         <a href="/tutor"><img className='logo' src="./images/address.png" alt='tutor'></img></a>
+    //         <p>Tutor</p>
+    //     </div>
+    //   </div>
      
   
-      <div className='infoLine date'>
-        <h3 className='title'>Date: </h3>
-        <h3 className='text'>Mon 09/11/2924</h3>
-      </div>
+    //   <div className='infoLine date'>
+    //     <h3 className='title'>Date: </h3>
+    //     <h3 className='text'>Mon 09/11/2924</h3>
+    //   </div>
   
-      <div className='infoLine startTime'>
-        <h3 className='title'>Start Time: </h3>
-        <h3 className='text'>12am PCT</h3>
-      </div>
+    //   <div className='infoLine startTime'>
+    //     <h3 className='title'>Start Time: </h3>
+    //     <h3 className='text'>12am PCT</h3>
+    //   </div>
   
-      <div className='infoLine endTime'>
-        <h3 className='title'>End Time: </h3>
-        <h3 className='text'>2am PCT</h3>
-      </div>
+    //   <div className='infoLine endTime'>
+    //     <h3 className='title'>End Time: </h3>
+    //     <h3 className='text'>2am PCT</h3>
+    //   </div>
   
-      <h4 id='message'>To start the meeting, enter Live Share URL</h4>
+    //   <h4 id='message'>To start the meeting, enter Live Share URL</h4>
       
-      {
-        (session.status === 'ongoing')? 
-        (
-            <div className='endSession'>
-                <button onClick={endSession}>End Session</button>
-            </div>
-        )
-         : (
-            <div className='startSession'>
-                <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
-                <button onClick={startSession}>Start Session</button>
-            </div>
-        )
+    //   {
+    //     (session.status === 'ongoing')? 
+    //     (
+    //         <div className='endSession'>
+    //             <button onClick={endSession}>End Session</button>
+    //         </div>
+    //     )
+    //      : (
+    //         <div className='startSession'>
+    //             <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
+    //             <button onClick={startSession}>Start Session</button>
+    //         </div>
+    //     )
 
-      }
+    //   }
       
-      {/* <h1>Tutor's View</h1>
-      <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
-      <button onClick={startSession}>Start Session</button>
-      <p>Live Share URL: {inputUrl}</p>
+    //   {/* <h1>Tutor's View</h1>
+    //   <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
+    //   <button onClick={startSession}>Start Session</button>
+    //   <p>Live Share URL: {inputUrl}</p>
       
-      <h1>Student's View</h1>
-      <button onClick={joinSession}>Join Session</button> */} 
-    </>
+    //   <h1>Student's View</h1>
+    //   <button onClick={joinSession}>Join Session</button> */} 
+    // </>
     )  
 }
 export default tutorInterface
