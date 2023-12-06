0c2adcc086b83a5a3962230d38a6b127cf7f457b
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-06 13:54:07 +0700

    add picture, fix design

--- client/src/MeetingSession/TestInterface.jsx
+++ client/src/MeetingSession/TestInterface.jsx
@@ -1,42 +1,41 @@
-import React from 'react'
-import '../css/TestInterface.css'
-
+import React from "react";
+import "../css/TestInterface.css";
+import testImg from "/src/assets/footer/circle.svg";
+import testImg1 from "/src/assets/footer/circle-small.svg";
+import user from "/src/assets/footer/circle-user.svg";
 function TestInterface() {
   return (
-    <div className='meetingSession'>
-      <div className='meetingInfo'>
-        <div className='line title'>
-          <img id='meetingInfoIcon' src='.../public/images/address.png'></img>
-          <h3>Meeting 1232</h3>
+    <div className="meetingSession">
+      <div className="meetingInfo">
+        <div className="line title">
+          <img className="pictureTitle" src={testImg} alt="circle" />
+          <h1>Meeting 1232</h1>
         </div>
-        <div className='line time'>
-          <img id='meetingInfoIcon' src='.../public/images/address.png'></img>
-          <h3>12:00 pm 12/12/2023</h3>
+        <div className="time line">
+          <img className="pictureTitle" src={testImg1} alt="circle" />
+          <h4>12:00 pm 12/12/2023</h4>
         </div>
-        <div className='line description'>
-          <img id='meetingInfoIcon' src='.../public/images/address.png'></img>
-          <h3>Learn React</h3>
+        <div className="line description">
+          <img className="pictureTitle" src={testImg1} alt="circle" />
+          <h4>Learn React</h4>
         </div>
-        <div className='line accessToURLs'>
-          <img id='meetingInfoIcon' src='.../public/images/address.png'></img>
-          <div className='buttons'>
-            <input
-                type="url"
-                placeholder="Live Share URL"
-            />
-            <button id='blueButton'>Start Session</button>
+        <div className="line accessToURLs">
+          <img className="pictureTitle" src={testImg1} alt="circle" />
+          <div className="buttons">
+            <input type="url" placeholder="Live Share URL" />
+            <button id="blueButton">Start Session</button>
           </div>
         </div>
       </div>
-      <div className='meetingParticipants'>
-        <h3>Participants:</h3>
-        <div className='participant'>
-          <img id='meetingInfoIcon' src='.../public/images/address.png'></img>
-          <h3>Participant 1</h3>
+      <div className="meetingParticipants">
+        <h2>Participants:</h2>
+        <div className="participant">
+          <img className="pictureTitle" src={user} alt="circle" />
+          <h4>Participant 1</h4>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TestInterface
+export default TestInterface;
