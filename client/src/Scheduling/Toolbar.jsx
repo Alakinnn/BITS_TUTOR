e434bb96a1127f123b4769f691b7787828ad0548
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-08 20:04:16 +0700

    adding html css for toolbar (description)

--- client/src/Scheduling/Toolbar.jsx
+++ client/src/Scheduling/Toolbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../css/Scheduling/Toolbar.css'
-
+import user from "/src/assets/footer/circle-user.svg";
 function Toolbar() {
   return (
     <div className='toolbar'>
@@ -28,13 +28,19 @@ function Toolbar() {
         {/* toolbar description */}
         <div className="toolbar-description">
             <div className="description-user-profile">
-                <img className='student-picture' src="" alt="" />
-                <div className="student-info"></div>
+                <img className="picture-user" src={user} alt="circle" />    
+                <div className="student-info">
+                    <div className="subject">Introduction to UI design</div>
+                    <div className="status">
+                        <div className="user-name">Nguyễn Hạ Uyên</div>
+                        <div className="current-time">2 minutes ago</div>
+                    </div>
+                </div>
             </div>
 
             {/* description time */}
             <div className="description-time">
-                <div className="request-date">Lorem ipsum dolor sit amet.</div>
+                <div className="request-date">Wednesday, December 6th</div>
                 
                 <div className="time">
                     <div className="from">
@@ -47,16 +53,15 @@ function Toolbar() {
                         <p>11:00</p>
                     </div>
                 </div>
-                
-                {/* content */}
-                <div className="content"></div>
+            </div>
+            {/* content */}
+            <input className='input-content' type="text" />
                 
                 {/* buttons */}
                 <div className="buttons">
                     <button className='approve'>Approve</button>
                     <button className='decline'>Decline</button>
                 </div>
-            </div>
         </div>
     </div> 
   )
