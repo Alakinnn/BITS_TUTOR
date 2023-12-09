4b92d4f4e663149827d22c4853f766d05b1d2a3d
LeBon985 <minhtoanpt2002@gmail.com>
2023-12-09 07:26:35 +0700

    Scheduling Toolbar
    Dashboard and View request in toolbar

--- client/src/Scheduling/Toolbar.jsx
+++ client/src/Scheduling/Toolbar.jsx
@@ -8,20 +8,58 @@ function Toolbar() {
         <div className="toolbar-menu">
             <div id="dashboard">
                 <img></img>
-                <p></p>
-            </div>
-            <div id="view-request">
-                <img></img>
-                <p></p>
+                <p>Dashboard</p>
             </div>
         </div>
         {/* toolbar request */}
         <div className="toolbar-requests">
-            <div className="request">
-                <img></img>
-                <div className="session-info">
-                    <p></p>
+        {/* <!-- view request --> */}
+            <div class="dropdown">
+                <div class="dropdown_select">
+                    <span class="dropdown_selected">
+                        <img src="image/interview.png" alt="" class="icon"></img>
+                        View Request
+                    </span>    
+                    <i class="fa fa-caret-down dropdown_caret"></i>
                 </div>
+                {/* <!-- Request notification --> */}
+                <ul class="dropdown_list">  
+                    <li class="dropdown_item">
+                        <span class="dropdown_text">
+                            <div class="left_column">
+                                <img src="image/dashboard.png" alt=""></img>
+                            </div>
+                            <div class="right_column">
+                                <div class="student_name">Nguyen Van A</div>
+                                <div class="course_name">Intro to Programming</div>
+                                <div class="meet_duration">10pm to 12pm</div>
+                            </div>
+                        </span>
+                    </li>
+                    <li class="dropdown_item">
+                        <span class="dropdown_text">
+                            <div class="left_column">
+                                <img src="image/dashboard.png" alt=""></img>
+                            </div>
+                            <div class="right_column">
+                                <div class="student_name">Nguyen Van A</div>
+                                <div class="course_name">Intro to Programming</div>
+                                <div class="meet_duration">10pm to 12pm</div>
+                            </div>
+                        </span>
+                    </li><li class="dropdown_item">
+                        <span class="dropdown_text">
+                            <div class="left_column">
+                                <img src="image/dashboard.png" alt=""></img>
+                            </div>
+                            <div class="right_column">
+                                <div class="student_name">Nguyen Van A</div>
+                                <div class="course_name">Intro to Programming</div>
+                                <div class="meet_duration">10pm to 12pm</div>
+                            </div>
+                        </span>
+                    </li>
+                </ul>
             </div>
         </div>
 
