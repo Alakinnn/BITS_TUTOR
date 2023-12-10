20c6970f6ff92e0b6e4dfa040b0ddfce1987d4da
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-12-11 00:20:03 +0700

    fixed getSession

--- server/src/controllers/session/getSession.ts
+++ server/src/controllers/session/getSession.ts
@@ -1,7 +1,7 @@
-import Session from "../../models/session";
+import Session, { SessionDoc } from "../../models/session";
 import { Request, Response } from "express";
 import { NotFoundError } from "../../errors";
-import { populateTutorAndStudent } from "../../utils/populate";
+import populateTutorAndStudent from "../../utils/populate";
 
 const getSessionById = async (req: Request, res: Response) => {
     const { sessionId } = req.params;
@@ -42,16 +42,16 @@ const getSessions = async (req: Request, res: Response) => {
 const getTutorSessions = async (req: Request, res: Response) => {
     const { tutorId } = req.params;
     const sessions = await Session.find({ tutor: tutorId });
-    await populateTutorAndStudent(sessions);
 
+    await Promise.all(sessions.map(async (session) => await populateTutorAndStudent(session)));
     return res.status(200).json(sessions);
 };
 
 const getStudentSessions = async (req: Request, res: Response) => {
     const { studentId } = req.params;
     const sessions = await Session.find({ student: studentId });
-    await populateTutorAndStudent(sessions);
 
+    await Promise.all(sessions.map(async (session) => await populateTutorAndStudent(session)));
     return res.status(200).json(sessions);
 };
 
