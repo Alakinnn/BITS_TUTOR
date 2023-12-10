cc723d124943e9727eacc6d73ee7432267ed86b2
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-12-10 20:45:58 +0700

    update event render function

--- client/src/Scheduling/SchedulingContainer.jsx
+++ client/src/Scheduling/SchedulingContainer.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import '../css/Scheduling/SchedulingContainer.css'
 import Toolbar from './Toolbar/Toolbar';
 import ScheduleView from './ScheduleView/ScheduleView';
+import { getTutorSessions } from './TutorScheduling';
 
 function SchedulingContainer() {
+  const [sessions, setSessions] = useState({});
+  useEffect(() => {
+    setSessions(getTutorSessions());
+  }, []);
   return (
     <div className='scheduling-container'>
       { <Toolbar /> }
-      { <ScheduleView /> }
+      { <ScheduleView
+        sessionList={sessions}
+      /> }
     </div>
   )
 }
--- client/src/Scheduling/TutorScheduling.jsx
+++ client/src/Scheduling/TutorScheduling.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import axios from 'axios'
 
-function TutorScheduling() {
-  return (
-    <div>TutorScheduling</div>
-  )
+const tutorId ='656f614ac37e79091ef39474'
+
+export async function getTutorSessions () {
+  const response = await axios.get(`http://139.59.105.114/api/v1/session/tutor/${tutorId}`);
+  // console.log(response.data);
+  return response.data;
 }
-
-export default TutorScheduling
