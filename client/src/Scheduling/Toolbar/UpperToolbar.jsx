d04327fa46431f74455709ef5696f9f95404bb81
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-12-11 15:00:04 +0700

    fix image src and formatDate bug

--- client/src/Scheduling/Toolbar/UpperToolbar.jsx
+++ client/src/Scheduling/Toolbar/UpperToolbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState } from 'react';
 import '../../css/Scheduling/Toolbar/UpperToolbar.css';
-// import dashboard from "/public/images/dashboard.png";    
-// import request from "/public/images/request.png";
-// import user from "/src/assets/footer/circle.svg";
+import dashboard from "/public/images/dashboard.png";    
+import request from "/public/images/request.png";
+import user from "/src/assets/footer/circle.svg";
 import '@fortawesome/fontawesome-free/css/all.css'; 
 
 const UpperToolbar = ({requestList}) => {
@@ -23,7 +23,7 @@ const UpperToolbar = ({requestList}) => {
             <div className="right_column">
                  <div className="student_name">{request.student.username}</div>
                 <div className="course_name">{request.title}</div>
-                <div className="meet_duration">{request.startTime} to {request.endTime}</div>
+                <div className="meet_duration">{formatDate(request.startTime)} to {formatDate(request.endTime)}</div>
             </div>
         </span>
         </li>
@@ -65,7 +65,7 @@ const UpperToolbar = ({requestList}) => {
 
   const formatDate = (dateString) => {
     // Convert the date string into a Date object
-    dateObj = new Date(dateString);
+    let dateObj = new Date(dateString);
     // Get the time portion of the Date object
     const timeString = dateObj.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
   
