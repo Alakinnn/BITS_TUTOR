7fe4822b6c1735146180b2081d0ff4d72242a258
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-12 23:21:05 +0700

    Finish MyTutor page

--- client/src/pages/ProfilePages/components/StudentPersonalDetail.jsx
+++ client/src/pages/ProfilePages/components/StudentPersonalDetail.jsx
@@ -1,39 +1,37 @@
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardText,
-    MDBRow,
-    MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
 } from "mdb-react-ui-kit";
 
 function StudentPersonalDetail() {
-    return (
-        <MDBCard className="mb-4">
-            <MDBCardBody>
-                <MDBRow>
-                    <MDBCol sm="3">
-                        <MDBCardText>Full Name</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                        <MDBCardText className="text-muted">
-                            Johnatan Smith
-                        </MDBCardText>
-                    </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                    <MDBCol sm="3">
-                        <MDBCardText>Email</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="9">
-                        <MDBCardText className="text-muted">
-                            example@example.com
-                        </MDBCardText>
-                    </MDBCol>
-                </MDBRow>
-            </MDBCardBody>
-        </MDBCard>
-    );
+  return (
+    <MDBCard className="mb-4">
+      <MDBCardBody>
+        <MDBRow>
+          <MDBCol sm="3">
+            <MDBCardText>Full Name</MDBCardText>
+          </MDBCol>
+          <MDBCol sm="9">
+            <MDBCardText className="text-muted">Johnatan Smith</MDBCardText>
+          </MDBCol>
+        </MDBRow>
+        <hr />
+        <MDBRow>
+          <MDBCol sm="3">
+            <MDBCardText>Email</MDBCardText>
+          </MDBCol>
+          <MDBCol sm="9">
+            <MDBCardText className="text-muted">
+              example@example.com
+            </MDBCardText>
+          </MDBCol>
+        </MDBRow>
+      </MDBCardBody>
+    </MDBCard>
+  );
 }
 
 export default StudentPersonalDetail;
