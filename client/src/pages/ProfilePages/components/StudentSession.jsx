2c88557f9ebd5c2b7e41327900fb1c83aab0745d
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-16 02:37:19 +0700

    Adding test html for student's session

--- client/src/pages/ProfilePages/components/StudentSession.jsx
+++ client/src/pages/ProfilePages/components/StudentSession.jsx
@@ -9,8 +9,8 @@ import {
   MDBListGroupItem,
   MDBIcon,
   MDBBtn,
-  // MDBRow,
-  // MDBCol,
+  MDBRow,
+  MDBCol,
 } from "mdb-react-ui-kit";
 // import Sessions from "./Sessions";
 
@@ -23,58 +23,53 @@ function StudentSession() {
           Sessions
         </MDBCardText>
         <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="chalkboard-teacher" size="lg" />
-          <MDBCardText className="mb-4">
-            <span className="text-primary font-italic me-1">
-              {" "}
-              Website Markup
-            </span>{" "}
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
-          </MDBCardText>
-          <MDBCardText className="mb-4">
+          <MDBRow>
+            <MDBCol sm="3">
+              <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+            </MDBCol>
+            <MDBCol sm="9">
+              <span className="text-primary font-italic me-1">
+                {" "}
+                Website Markup
+              </span>{" "}
+              <MDBCardText className="text-style">Start on: 9:00pm</MDBCardText>
+            </MDBCol>
+          </MDBRow>
+          <MDBCardText className="mb-4 button-style">
             <MDBBtn size="sm" className="bg-secondary ">
               Go to the Session
             </MDBBtn>
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
+            <MDBCardText className="small-text">16/12/2023</MDBCardText>
           </MDBCardText>
         </MDBListGroupItem>
+
         <hr />
+
         <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="chalkboard-teacher" size="lg" />
-          <MDBCardText className="mb-4">
-            <span className="text-primary font-italic me-1">
-              {" "}
-              Website Markup
-            </span>{" "}
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
-          </MDBCardText>
-          <MDBCardText className="mb-4">
-            <MDBBtn size="sm" className="bg-secondary ">
+          <MDBRow>
+            <MDBCol sm="3">
+              <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+            </MDBCol>
+            <MDBCol sm="9">
+              <span className="text-primary font-italic me-1">
+                {" "}
+                Website Markup
+              </span>{" "}
+              <MDBCardText className="text-style">Start on: 9:00pm</MDBCardText>
+            </MDBCol>
+          </MDBRow>
+          <MDBCardText className="mb-4 button-style">
+          <MDBBtn size="sm" className="bg-secondary ">
               Go to the Session
             </MDBBtn>
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
+            <MDBCardText className="small-text">16/12/2023</MDBCardText>
           </MDBCardText>
         </MDBListGroupItem>
+
         <hr />
+        
         <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="chalkboard-teacher" size="lg" />
-          <MDBCardText className="mb-4">
-            <span className="text-primary font-italic me-1">
-              {" "}
-              Website Markup
-            </span>{" "}
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
-          </MDBCardText>
-          <MDBCardText className="mb-4">
-            <MDBBtn size="sm" className="bg-secondary ">
-              Go to the Session
-            </MDBBtn>
-            <MDBCardText className="small-text">mdbootstrap</MDBCardText>
-          </MDBCardText>
-        </MDBListGroupItem>
-        <hr />
-        <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+          <MDBIcon fas icon="chalkboard-teacher" size="lg" />
           <MDBCardText className="mb-4">
             <span className="text-primary font-italic me-1">
               {" "}
