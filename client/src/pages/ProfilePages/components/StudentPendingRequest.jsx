bf20156a239a7c645091ee71843e6c2f90abff0a
Ngoan <72678683+ngoan22lbt@users.noreply.github.com>
2023-12-16 22:47:38 +0700

    add css for responsive ( session and pending request)

--- client/src/pages/ProfilePages/components/StudentPendingRequest.jsx
+++ client/src/pages/ProfilePages/components/StudentPendingRequest.jsx
@@ -12,35 +12,85 @@ import {
 
 function StudentPendingRequest() {
   return (
-    <MDBCard className="mb-4 mb-md-0">
+    <MDBCard className="mb-4 mb-md-0 card-session">
       <MDBCardBody>
         <MDBCardText className="mb-4">
           <span className="text-primary font-italic me-1">Student</span> Pending
           Request
         </MDBCardText>
-        <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="user-circle" size="lg" />
-          <MDBCardText>Nguyen Ngoan</MDBCardText>
-          <MDBBtn className="bg-secondary">Pending Request</MDBBtn>
+        <MDBListGroupItem className="d-flex justify-content-between align-items-center">
+          <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+          <MDBCardText className="card-text">
+            <span className="text-primary font-italic me-1 style-title">
+              {" "}
+              Website Markup
+            </span>{" "}
+            <MDBCardText className="small-text">
+              Your request sent 5 minutes ago
+            </MDBCardText>
+          </MDBCardText>
+          <MDBCardText>
+            <MDBBtn size="sm" className="button-session bg-secondary  ">
+              Pending
+            </MDBBtn>
+          </MDBCardText>
         </MDBListGroupItem>
         <hr />
-        <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="user-circle" size="lg" />
-          <MDBCardText>Nguyen Ngoan</MDBCardText>
-          <MDBBtn className="bg-secondary">Pending Request</MDBBtn>
+
+        <MDBListGroupItem className="d-flex justify-content-between align-items-center">
+          <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+          <MDBCardText className="card-text">
+            <span className="text-primary font-italic me-1 style-title">
+              {" "}
+              Website Markup
+            </span>{" "}
+            <MDBCardText className="small-text">
+              Your request sent 5 minutes ago
+            </MDBCardText>
+          </MDBCardText>
+          <MDBCardText>
+            <MDBBtn size="sm" className="button-session bg-secondary  ">
+              Pending
+            </MDBBtn>
+          </MDBCardText>
+        </MDBListGroupItem>
+
+        <hr />
+
+        <MDBListGroupItem className="d-flex justify-content-between align-items-center">
+          <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+          <MDBCardText className="card-text">
+            <span className="text-primary font-italic me-1 style-title">
+              {" "}
+              Website Markup
+            </span>{" "}
+            <MDBCardText className="small-text">
+              Your request sent 5 minutes ago
+            </MDBCardText>
+          </MDBCardText>
+          <MDBCardText>
+            <MDBBtn size="sm" className="button-session bg-secondary  ">
+              Pending
+            </MDBBtn>
+          </MDBCardText>
         </MDBListGroupItem>
         <hr />
-        <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-        <MDBIcon fas icon="user-circle" size="lg" />
-        <MDBCardText>Nguyen Ngoan</MDBCardText>
-          <MDBBtn className="bg-secondary">Pending Request</MDBBtn>
-        </MDBListGroupItem>
-        <hr />
-        <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
-          <MDBIcon fas icon="user-circle" size="lg" />
-          <MDBCardText>Nguyen Ngoan</MDBCardText>
-          
-          <MDBBtn className="bg-secondary">Pending Request</MDBBtn>
+        <MDBListGroupItem className="d-flex justify-content-between align-items-center">
+          <MDBIcon fas icon="chalkboard-teacher" size="lg" />
+          <MDBCardText className="card-text">
+            <span className="text-primary font-italic me-1 style-title">
+              {" "}
+              Website Markup
+            </span>{" "}
+            <MDBCardText className="small-text">
+              Your request sent 5 minutes ago
+            </MDBCardText>
+          </MDBCardText>
+          <MDBCardText>
+            <MDBBtn size="sm" className="button-session bg-secondary  ">
+              Pending
+            </MDBBtn>
+          </MDBCardText>
         </MDBListGroupItem>
       </MDBCardBody>
     </MDBCard>
