4f56348bd6163f33a54c02716a6ccd6224e2a675
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-12-17 18:59:19 +0700

    update rearrange data
    Something got error, will fix tonight

--- client/src/pages/AuthenticationPages/Register/StudentRegister.jsx
+++ client/src/pages/AuthenticationPages/Register/StudentRegister.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import LoginPage, {
-    Username,
-    Password,
-    Submit,
-    Title,
-    Banner,
+  Username,
+  Password,
+  Submit,
+  Title,
+  Banner,
 } from "@react-login-page/page11";
 import "react-image-upload/dist/index.css";
 import "../styles/ImageUpload.css";
@@ -12,78 +12,77 @@ import ProfilePicUpload from "../components/ProfilePicUpload";
 import SocialLinksList from "../components/SocialLinksList";
 
 const StudentRegisterPage = () => {
-    const [data, setData] = React.useState({});
-    const [arrangedData, setArrangedData] = React.useState({}); // Create a copy of the original data
-    const reArrangeData = () => {
-        const socialLinks = [];
-        setArrangedData(data);
-        // Loop through the keys in the data object
-        Object.keys(arrangedData).forEach((key) => {
-            // Check if the key starts with "socialLink-"
-            if (key.startsWith("socialLink-")) {
-                socialLinks.push(arrangedData[key]); // Push social links to the socialLinks array
-                delete arrangedData[key]; // Remove the social link key from the newData object
-            }
-        });
+  const [data, setData] = React.useState({});
+  const [arrangedData, setArrangedData] = React.useState({}); // Create a copy of the original data
 
-        setArrangedData({ ...data, socialLinks: socialLinks }); // Add the socialLinks array to the newData object
-    };
+  const reArrangeData = (newData) => {
+    const socialLinks = [];
+    setArrangedData(newData);
+    // Loop through the keys in the data object
+    Object.keys(arrangedData).forEach((key) => {
+      // Check if the key starts with "socialLink-"
+      if (key.startsWith("socialLink-")) {
+        socialLinks.push(arrangedData[key]); // Push social links to the socialLinks array
+        delete arrangedData[key]; // Remove the social link key from the newData object
+      }
+    });
+    setArrangedData({ ...newData, socialLinks: socialLinks }); // Add the socialLinks array to the newData object
+  };
 
-    const [socialLinks, setSocialLinks] = React.useState([null]);
+  const [socialLinks, setSocialLinks] = React.useState([null]);
 
-    const handleAdd = () => {
-        const newSocialLinks = [...socialLinks, " "]; // Create a new input
-        setSocialLinks(newSocialLinks); // Update the state with the new inputs
-    };
+  const handleAdd = () => {
+    const newSocialLinks = [...socialLinks, " "]; // Create a new input
+    setSocialLinks(newSocialLinks); // Update the state with the new inputs
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const newData = Object.fromEntries(formData);
-        setData(newData);
-        console.log(data);
-        reArrangeData();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newData = Object.fromEntries(formData);
+    setData(newData);
+    reArrangeData(newData);
 
-        console.log("Arranged data", arrangedData);
-    };
+    console.log("Arranged data", arrangedData);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <LoginPage>
-                <ProfilePicUpload index={0} />
-                <Username
-                    index={3}
-                    label="Full Name"
-                    name="username"
-                    placeholder="Your Full Name"
-                />
-                <Password
-                    index={4}
-                    label="Password"
-                    placeholder="Password"
-                    name="password"
-                />
+  return (
+    <form onSubmit={handleSubmit}>
+      <LoginPage>
+        <ProfilePicUpload index={0} />
+        <Username
+          index={3}
+          label="Full Name"
+          name="username"
+          placeholder="Your Full Name"
+        />
+        <Password
+          index={4}
+          label="Password"
+          placeholder="Password"
+          name="password"
+        />
 
-                <Submit type="submit">Confirm</Submit>
+        <Submit type="submit">Confirm</Submit>
 
-                <Username keyname="username_rule" visible={false} index={5}>
-                    Social Links
-                </Username>
+        <Username keyname="username_rule" visible={false} index={5}>
+          Social Links
+        </Username>
 
-                <SocialLinksList
-                    baseIndex={6}
-                    socialLinks={socialLinks}
-                    handleAdd={handleAdd}
-                />
+        <SocialLinksList
+          baseIndex={6}
+          socialLinks={socialLinks}
+          handleAdd={handleAdd}
+        />
 
-                <Banner>
-                    <img src="https://hips.hearstapps.com/hmg-prod/images/kevin-main-image-1579114576.png"></img>
-                </Banner>
+        <Banner>
+          <img src="https://hips.hearstapps.com/hmg-prod/images/kevin-main-image-1579114576.png"></img>
+        </Banner>
 
-                <Title visible={false} />
-            </LoginPage>
-        </form>
-    );
+        <Title visible={false} />
+      </LoginPage>
+    </form>
+  );
 };
 
 export default StudentRegisterPage;
