43199cfd9ba3553bb2b9d300e0fcbea8670b5043
MasterMobo <mastermobo101@gmail.com>
2023-12-19 16:59:37 +0700

    changed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,52 +1,101 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import {
+    MDBContainer,
     MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
     MDBNavbarNav,
     MDBNavbarItem,
     MDBNavbarLink,
-    MDBNavbarToggler,
-    MDBContainer,
-    MDBIcon,
+    MDBBtn,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
     MDBCollapse,
 } from "mdb-react-ui-kit";
 
-function Header() {
-    const [showBasic, setShowBasic] = useState(false);
+export default function App() {
+    const [openBasic, setOpenBasic] = useState(false);
 
     return (
         <header>
-            <MDBNavbar expand="lg" light bgColor="white">
+            <MDBNavbar expand="lg" light bgColor="light">
                 <MDBContainer fluid>
+                    <MDBNavbarBrand href="#">Brand</MDBNavbarBrand>
+
                     <MDBNavbarToggler
-                        onClick={() => setShowBasic(!showBasic)}
-                        aria-controls="navbarExample01"
+                        aria-controls="navbarSupportedContent"
                         aria-expanded="false"
                         aria-label="Toggle navigation"
+                        onClick={() => setOpenBasic(!openBasic)}
                     >
-                        <MDBIcon fas icon="bars" />
+                        <MDBIcon icon="bars" fas />
                     </MDBNavbarToggler>
-                    <MDBCollapse navbar show={showBasic}>
-                        <MDBNavbarNav right className="mb-2 mb-lg-0">
-                            <MDBNavbarItem active>
-                                <MDBNavbarLink aria-current="page" href="#">
+
+                    <MDBCollapse navbar open={openBasic}>
+                        <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+                            <MDBNavbarItem>
+                                <MDBNavbarLink
+                                    active
+                                    aria-current="page"
+                                    href="#"
+                                >
                                     Home
                                 </MDBNavbarLink>
                             </MDBNavbarItem>
                             <MDBNavbarItem>
-                                <MDBNavbarLink href="#">Features</MDBNavbarLink>
+                                <MDBNavbarLink href="#">Link</MDBNavbarLink>
                             </MDBNavbarItem>
+
                             <MDBNavbarItem>
-                                <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
+                                <MDBDropdown>
+                                    <MDBDropdownToggle
+                                        tag="a"
+                                        className="nav-link"
+                                        role="button"
+                                    >
+                                        Dropdown
+                                    </MDBDropdownToggle>
+                                    <MDBDropdownMenu>
+                                        <MDBDropdownItem link>
+                                            Action
+                                        </MDBDropdownItem>
+                                        <MDBDropdownItem link>
+                                            Another action
+                                        </MDBDropdownItem>
+                                        <MDBDropdownItem link>
+                                            Something else here
+                                        </MDBDropdownItem>
+                                    </MDBDropdownMenu>
+                                </MDBDropdown>
                             </MDBNavbarItem>
+
                             <MDBNavbarItem>
-                                <MDBNavbarLink href="#">About</MDBNavbarLink>
+                                <MDBNavbarLink
+                                    disabled
+                                    href="#"
+                                    tabIndex={-1}
+                                    aria-disabled="true"
+                                >
+                                    Disabled
+                                </MDBNavbarLink>
                             </MDBNavbarItem>
                         </MDBNavbarNav>
+
+                        <form className="d-flex input-group w-auto">
+                            <input
+                                type="search"
+                                className="form-control"
+                                placeholder="Type query"
+                                aria-label="Search"
+                            />
+                            <MDBBtn color="primary">Search</MDBBtn>
+                        </form>
                     </MDBCollapse>
                 </MDBContainer>
             </MDBNavbar>
         </header>
     );
 }
-
-export default Header;
