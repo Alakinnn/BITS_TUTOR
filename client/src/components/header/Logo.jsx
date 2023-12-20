231930ea8ea7d2167bdb0d902e479e346acfe67c
MasterMobo <mastermobo101@gmail.com>
2023-12-20 22:40:14 +0700

    added logo

--- client/src/components/header/Logo.jsx
+++ client/src/components/header/Logo.jsx
@@ -1,7 +1,25 @@
 import { MDBNavbarBrand } from "mdb-react-ui-kit";
-
+import logo from "/images/mam_logo.png";
+// import logo from "/images/mam_logo_words.svg";
 function Logo() {
-    return <MDBNavbarBrand href="/">Brand</MDBNavbarBrand>;
+    return (
+        <MDBNavbarBrand
+            style={{
+                width: "70px",
+                height: "60px",
+            }}
+            href="/"
+        >
+            <img
+                style={{
+                    width: "100%",
+                    marginLeft: "10px",
+                }}
+                src={logo}
+                alt="logo"
+            />
+        </MDBNavbarBrand>
+    );
 }
 
 export default Logo;
