866ba9f01554b9ec6a366059a64d156177e5bb3e
LeBon985 <minhtoanpt2002@gmail.com>
2023-12-20 22:23:26 +0700

    filter
    filter

--- client/src/pages/SearchResults/components/ResultFilter.jsx
+++ client/src/pages/SearchResults/components/ResultFilter.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "/src/css/SearchResults/SearchResults.css";
+import {filterFunction} from "./FilterFunction.js";
 
 function ResultFilter({ user }) {
+  useEffect(() => {
+    filterFunction();
+  }, []);
   return (
-    <div className="col-md-3 col-md-push-9">
+    <div className="filter col-md-3 col-md-push-9">
       <h4>
         Results <span className="fw-semi-bold">Filtering</span>
       </h4>
-      <p className="text-muted fs-mini">
+      {/* <p className="text-muted fs-mini">
         Listed content is categorized by the following groups:
       </p>
       <ul className="nav nav-pills nav-stacked search-result-categories mt">
@@ -32,7 +36,55 @@ function ResultFilter({ user }) {
             Globals <span className="badge">18</span>
           </a>
         </li>
+      </ul> */}
+       <div class="dropdown">
+      <div class="select">
+        <div class="selected">Name</div>
+        <div class="caret"></div>
+      </div>
+      <ul class="menu">
+        <li class="active">default</li>
+        <li>xyz</li>
+        <li>zxc</li>
       </ul>
+    </div>
+    <div class="wrapper">
+      <header>
+        <p>Use slider or enter min and max price</p>
+      </header>
+      <div class="price-input">
+        <div class="field">
+          <span>Min</span>
+          <input type="number" class="input-min" value="2500" />
+        </div>
+        <div class="separator">-</div>
+        <div class="field">
+          <span>Max</span>
+          <input type="number" class="input-max" value="7500" />
+        </div>
+      </div>
+      <div class="slider">
+        <div class="progress"></div>
+      </div>
+      <div class="range-input">
+        <input
+          type="range"
+          class="range-min"
+          min="0"
+          max="10000"
+          value="2500"
+          step="100"
+        />
+        <input
+          type="range"
+          class="range-max"
+          min="0"
+          max="10000"
+          value="7500"
+          step="100"
+        />
+      </div>
+    </div>
     </div>
   );
 }
