437caccf298f9a46f6db14ff88e63dce8c4bbc78
MasterMobo <mastermobo101@gmail.com>
2023-12-23 15:25:13 +0700

    updated core values text

---
+++ client/src/pages/HomePages/components/CoreValues.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function CoreValues() {
+    return (
+        <section className="container mt-5 mb-5">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Our Core Values</h1>
+                </div>
+                <div className="col mt-5">
+                    <p>Your education is invaluable to us</p>
+                </div>
+            </div>
+
+            <div className="row mt-5">
+                <div className="col-4">
+                    <h3>Interactivity</h3>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Sunt sapiente doloribus debitis, earum cumque
+                        obcaecati vero odit nisi culpa distinctio impedit eum
+                        dolores labore molestiae placeat at, ipsum veniam
+                        quibusdam!
+                    </p>
+                </div>
+                <div className="col-4">
+                    <h3>Personalization</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Est itaque blanditiis nesciunt labore minima dolorem
+                        doloremque nam expedita, quisquam, alias neque
+                        reprehenderit unde ab nihil adipisci aliquam fugit
+                        aperiam voluptas!
+                    </p>
+                </div>
+                <div className="col-4">
+                    <h3>Trust</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Velit porro veritatis pariatur. Nihil delectus adipisci
+                        ad mollitia, earum vitae maxime, illo ipsa corporis
+                        nobis maiores veritatis consequuntur illum. Animi,
+                        nihil?
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default CoreValues;
