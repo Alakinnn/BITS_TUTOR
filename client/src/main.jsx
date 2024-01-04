1cb380230dc5ed0ec67880f483d23a9b71407bf1
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2024-01-04 20:35:00 +0700

    Vercel speedinsights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.jsx";
 import "./css/index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Provider } from "react-redux";
 import { store } from "./store.js";
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <SpeedInsights />
     </Provider>
   </React.StrictMode>
 );
